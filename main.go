dab508adfafc510640472b2076367602608a8ad0
Tomasz Jóźwik <dev@entire.studio>
2022-11-29 11:15:40 +0100

    Replace duplicated prints with a loop over map

--- main.go
+++ main.go
@@ -10,15 +10,6 @@ import (
 
 const ApiBaseUri = "http://localhost:8000"
 
-type T struct {
-	Memory struct {
-		Total string `json:"total"`
-		Used  string `json:"used"`
-		Free  string `json:"free"`
-	} `json:"memory"`
-	Ip string `json:"ip"`
-}
-
 func main() {
 	resp, err := http.Get(ApiBaseUri + "/api")
 	if err != nil {
@@ -30,13 +21,25 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	var jsonData T
+	var jsonData struct {
+		Memory struct {
+			Total string `json:"total"`
+			Used  string `json:"used"`
+			Free  string `json:"free"`
+		} `json:"memory"`
+		Ip string `json:"ip"`
+	}
 	if err := json.Unmarshal(body, &jsonData); err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println("IP: ", jsonData.Ip)
-	fmt.Println("Total memory: ", jsonData.Memory.Total)
-	fmt.Println("Free memory: ", jsonData.Memory.Free)
-	fmt.Println("Used memory: ", jsonData.Memory.Used)
+	var properties = map[string]string{
+		"IP":           jsonData.Ip,
+		"Total memory": jsonData.Memory.Total,
+		"Free memory":  jsonData.Memory.Free,
+		"Used memory":  jsonData.Memory.Used,
+	}
+	for name, value := range properties {
+		fmt.Println(name+":", value)
+	}
 }
